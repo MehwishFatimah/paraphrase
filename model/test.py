23a41826ea32bbd48f14524f943a9117c27ca14a
Sarah Widder <sarah.j.widder@gmail.com>
2019-12-05 13:49:02 -0500

    fix inputs

--- model/test.py
+++ model/test.py
@@ -19,28 +19,29 @@ from model import EncoderRNN, BiLSTM, AttnDecoderRNN, prepareData, evaluate
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-input_lang, output_lang, supertag_lang, pairs = prepareData('ref', 'para', test=False)
-teacher_forcing_ratio = 0.5
-hidden_size = 256
+if __name__ == '__main__':
+    input_lang, output_lang, supertag_lang, pairs = prepareData('ref', 'para', test=False)
+    teacher_forcing_ratio = 0.5
+    hidden_size = 256
 
 
-encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
-encoder1.load_state_dict(torch.load('12-5-19/encoder_step_75000.pt'))
-encoder1.eval()
+    encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
+    encoder1.load_state_dict(torch.load('12-5-19/encoder_step_75000.pt'))
+    encoder1.eval()
 
-supertag_encoder1 = BiLSTM(supertag_lang.n_words, hidden_size).to(device)
-supertag_encoder1.load_state_dict(torch.load('12-5-19/supertage_encoder_step_75000.pt'))
-supertag_encoder1.eval()
+    supertag_encoder1 = BiLSTM(supertag_lang.n_words, hidden_size).to(device)
+    supertag_encoder1.load_state_dict(torch.load('12-5-19/supertag_encoder_step_75000.pt'))
+    supertag_encoder1.eval()
 
-attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
-attn_decoder1.load_state_dict(torch.load('12-5-19/decoder_step_75000.pt'))
-attn_decoder1.eval()
+    attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+    attn_decoder1.load_state_dict(torch.load('12-5-19/decoder_step_75000.pt'))
+    attn_decoder1.eval()
 
-test_input, test_output, test_supertags, test_pairs = prepareData('test-ref', 'test-para', test=True)
+    test_input, test_output, test_supertags, test_pairs = prepareData('test-ref', 'test-para', test=True)
 
 
-with open('12-5-19/test_output.txt', 'w') as f:
-    for pair in test_pairs:
-        output_words, attentions = evaluate(encoder1, supertag_encoder1, attn_decoder1, pair[0], pair[2])
-        output_sentence = ' '.join(output_words)
-        f.write(output_sentence + '\n')
+    with open('12-5-19/test_output.txt', 'w') as f:
+        for pair in test_pairs:
+            output_words, attentions = evaluate(encoder1, supertag_encoder1, attn_decoder1, pair[0], pair[2], input_lang, supertag_lang)
+            output_sentence = ' '.join(output_words)
+            f.write(output_sentence + '\n')
