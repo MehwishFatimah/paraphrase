58d9ca3828d116c8e4f63acee60165311d5e9bbf
Sarah Widder <sarah.j.widder@gmail.com>
2020-04-27 14:18:19 -0500

    grammar update

--- model/grammar.py
+++ model/grammar.py
@@ -109,6 +109,20 @@ verb_pairs = [
     VerbPair('complimented', 'was complimented by')
 ]
 
+short_verb_pairs = [
+    VerbPair('ate', 'eaten'),
+    VerbPair('liked', 'liked'),
+    VerbPair('loved', 'loved'),
+    VerbPair('kicked', 'kicked'),
+    VerbPair('bit', 'bitten'),
+    VerbPair('tapped', 'tapped'),
+    VerbPair('hated', 'hated'),
+    VerbPair('disliked', 'disliked'),
+    VerbPair('hunted', 'hunted'),
+    VerbPair('stalked', 'stalked'),
+    VerbPair('complimented', 'complimented')
+]
+
 modal_pairs = [
     VerbPair('must', 'needs to'),
     VerbPair('must', 'has got to'),
@@ -117,12 +131,21 @@ modal_pairs = [
     VerbPair('needs to', 'should')
 ]
 
+shortened_modal_pairs = [
+    VerbPair('must', 'needs'),
+    VerbPair('must', 'got'),
+    VerbPair('needs', 'ought'),
+    VerbPair('must', 'ought'),
+    VerbPair('needs', 'should')
+]
+modals = ['must', 'needs', 'got', 'ought', 'should']
+
 infinitives = [ 'eat', 'like', 'love', 'kick', 'bite', 'tap', 'hate', 'dislike', 'hunt', 'stalk', 'compliment']
 
-active_verbs = [verb.active for verb in verb_pairs]
-passive_verbs = [verb.passive for verb in verb_pairs]
+active_verbs = [verb.active for verb in short_verb_pairs]
+passive_verbs = [verb.passive for verb in short_verb_pairs]
 
-verbs = active_verbs + passive_verbs
+verbs = active_verbs + passive_verbs + infinitives
 
 lexicon = [Word('N', w) for w in agent_nouns] + \
             [Word('N_loc', w) for w in location_nouns] + \
@@ -130,7 +153,6 @@ lexicon = [Word('N', w) for w in agent_nouns] + \
             [Word('D', w) for w in determiners] + \
             [Word('Adj', w) for w in adjectives] + \
             [Word('P', w) for w in prepositions] + [Word(',', ',')]
-
 
 
 def generate_tree(start, parent, lexicon):
@@ -245,7 +267,7 @@ def generate_move_pp_pair():
     
     return (s1, s2)
 
-def generate_modal_pair():
+def generate_modal_pair(allow_pp=True):
     sent1 = generate_tree('S', None, lexicon)
     sent2 = sent1.copy()
     v1 = sent1.children[1].children[0]
@@ -257,11 +279,12 @@ def generate_modal_pair():
     v1.children = [Constituent(modal_pair.active + " " + infinitive, v1, is_leaf=True)]
     v2.children = [Constituent(modal_pair.passive + " " + infinitive, v2, is_leaf=True)]
 
-    r = np.random.uniform()
-    if r < 0.2:
-        sent1 = add_pp_front(sent1)
-    elif r < 0.4:
-        sent1 = add_pp_end(sent1)
+    if allow_pp:
+        r = np.random.uniform()
+        if r < 0.2:
+            sent1 = add_pp_front(sent1)
+        elif r < 0.4:
+            sent1 = add_pp_end(sent1)
     
     return (sent1, sent2)
 
@@ -318,6 +341,8 @@ def generate_act_pass_pairs(n, path):
             with open(path + 'act-pass-para.txt', 'w') as para:
                 for _ in range(n):
                     s1, s2 = generate_act_pass_pair()
+                    while len(s1.sentence().split(' ')) >= 15 or len(s2.sentence().split(' ')) >= 15:
+                        s1, s2 = generate_act_pass_pair()
                     s1 = s1.sentence()
                     s2 = s2.sentence()
                     pairs.write(s1 + '\t' + s2 + '\n')
@@ -329,4 +354,50 @@ def generate_act_pass_pairs(n, path):
                     para.write(s2 + '\n')
                     para.write(s1 + '\n')
 
-generate_act_pass_pairs(25000, '../data/artificial-data/set-2/active-passive/')
+# generate_act_pass_pairs(25000, '../data/artificial-data/set-2/active-passive/')
+
+def generate_modal_pairs(n, path):
+    with open(path + 'modal-pairs.txt', 'w') as pairs:
+        with open(path + 'modal-ref.txt', 'w') as ref:
+            with open(path + 'modal-para.txt', 'w') as para:
+                for _ in range(n):
+                    s1, s2 = generate_modal_pair(allow_pp=False)
+                    while len(s1.sentence().split(' ')) >= 15 or len(s2.sentence().split(' ')) >= 15:
+                        s1, s2 = generate_modal_pair(allow_pp=False)
+                    
+                    s1 = s1.sentence()
+                    s2 = s2.sentence()
+
+                    pairs.write(s1 + '\t' + s2 + '\n')
+                    pairs.write(s2 + '\t' + s1 + '\n')
+
+                    ref.write(s1 + '\n')
+                    ref.write(s2 + '\n')
+
+                    para.write(s2 + '\n')
+                    para.write(s1 + '\n')
+
+# generate_modal_pairs(25000, '../data/artificial-data/set-2/modal/')
+
+# s1, s2 = generate_move_pp_pair()
+# print(s1.sentence(), s2.sentence())
+
+
+def get_synonym_list():
+    syn_list = set()
+    for word in agent_nouns:
+        synonym = most_common_syn(word, wn.NOUN)
+        syn_list.add((word, synonym))
+    for word in location_nouns:
+        synonym = most_common_syn(word, wn.NOUN)
+        syn_list.add((word, synonym))
+    for word in adjectives:
+        synonym = most_common_syn(word, wn.ADJ)
+        syn_list.add((word, synonym))
+    for i in range(len(short_verb_pairs)):
+        pair = short_verb_pairs[i]
+        inf = infinitives[i]
+        syn_list.add((pair.active, pair.passive, inf))
+    for pair in shortened_modal_pairs:
+        syn_list.add((pair.active, pair.passive))
+    return syn_list
